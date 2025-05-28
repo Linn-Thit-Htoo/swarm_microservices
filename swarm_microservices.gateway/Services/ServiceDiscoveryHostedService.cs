f0dcb7b5b5427da4ff6f2bca2a8546c9b498d2f5
Lin Thit Htoo <lth1212001@gmail.com>
2025-05-28 15:39:51 +0630

    -

--- swarm_microservices.gateway/Services/ServiceDiscoveryHostedService.cs
+++ swarm_microservices.gateway/Services/ServiceDiscoveryHostedService.cs
@@ -1,53 +1,52 @@
 ﻿using Consul;
 using swarm_microservices.gateway.Configurations;
 
-namespace swarm_microservices.gateway.Services
+namespace swarm_microservices.gateway.Services;
+
+public class ServiceDiscoveryHostedService : IHostedService
 {
-    public class ServiceDiscoveryHostedService : IHostedService
+    private readonly IConsulClient _client;
+    private readonly ServiceConfig _config;
+    private AgentServiceRegistration _registration;
+
+    public ServiceDiscoveryHostedService(IConsulClient client, ServiceConfig config)
     {
-        private readonly IConsulClient _client;
-        private readonly ServiceConfig _config;
-        private AgentServiceRegistration _registration;
+        _client = client;
+        _config = config;
+    }
 
-        public ServiceDiscoveryHostedService(IConsulClient client, ServiceConfig config)
+    public async Task StartAsync(CancellationToken cancellationToken)
+    {
+        _registration = new AgentServiceRegistration
         {
-            _client = client;
-            _config = config;
-        }
-
-        public async Task StartAsync(CancellationToken cancellationToken)
-        {
-            _registration = new AgentServiceRegistration
+            ID = _config.Id,
+            Name = _config.Name,
+            Address = _config.Address,
+            Port = _config.Port,
+            Check = new AgentServiceCheck()
             {
-                ID = _config.Id,
-                Name = _config.Name,
-                Address = _config.Address,
-                Port = _config.Port,
-                Check = new AgentServiceCheck()
-                {
-                    DeregisterCriticalServiceAfter = TimeSpan.FromSeconds(5),
-                    Interval = TimeSpan.FromSeconds(10),
-                    HTTP = _config.HealthCheckEndPoint,
-                    Timeout = TimeSpan.FromSeconds(10),
-                },
-            };
+                DeregisterCriticalServiceAfter = TimeSpan.FromSeconds(5),
+                Interval = TimeSpan.FromSeconds(10),
+                HTTP = _config.HealthCheckEndPoint,
+                Timeout = TimeSpan.FromSeconds(10),
+            },
+        };
 
-            // Deregister already registered service
-            await _client
-                .Agent.ServiceDeregister(_registration.ID, cancellationToken)
-                .ConfigureAwait(false);
+        // Deregister already registered service
+        await _client
+            .Agent.ServiceDeregister(_registration.ID, cancellationToken)
+            .ConfigureAwait(false);
 
-            // Registers service
-            await _client.Agent.ServiceRegister(_registration, cancellationToken).ConfigureAwait(false);
-        }
+        // Registers service
+        await _client.Agent.ServiceRegister(_registration, cancellationToken).ConfigureAwait(false);
+    }
 
-        public async Task StopAsync(CancellationToken cancellationToken)
-        {
-            var registration = new AgentServiceRegistration { ID = _config.Id };
+    public async Task StopAsync(CancellationToken cancellationToken)
+    {
+        var registration = new AgentServiceRegistration { ID = _config.Id };
 
-            await _client
-                .Agent.ServiceDeregister(_registration.ID, cancellationToken)
-                .ConfigureAwait(false);
-        }
+        await _client
+            .Agent.ServiceDeregister(_registration.ID, cancellationToken)
+            .ConfigureAwait(false);
     }
 }
