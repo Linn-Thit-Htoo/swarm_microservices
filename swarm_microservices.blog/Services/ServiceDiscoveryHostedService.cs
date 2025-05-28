54b4a9fbfbc1c42655bfb4bf33cd984274732a03
Lin Thit Htoo <lth1212001@gmail.com>
2025-05-28 15:39:10 +0630

    -

--- swarm_microservices.blog/Services/ServiceDiscoveryHostedService.cs
+++ swarm_microservices.blog/Services/ServiceDiscoveryHostedService.cs
@@ -1,62 +1,61 @@
 ﻿using Consul;
 using swarm_microservices.auth.Configurations;
 
-namespace swarm_microservices.auth.Services
+namespace swarm_microservices.auth.Services;
+
+public class ServiceDiscoveryHostedService : IHostedService
 {
-    public class ServiceDiscoveryHostedService : IHostedService
+    private readonly IConsulClient _client;
+    private readonly ServiceConfig _config;
+    private AgentServiceRegistration _registration;
+    private readonly ILogger<ServiceDiscoveryHostedService> _logger;
+
+    public ServiceDiscoveryHostedService(IConsulClient client, ServiceConfig config, ILogger<ServiceDiscoveryHostedService> logger)
     {
-        private readonly IConsulClient _client;
-        private readonly ServiceConfig _config;
-        private AgentServiceRegistration _registration;
-        private readonly ILogger<ServiceDiscoveryHostedService> _logger;
+        _client = client;
+        _config = config;
+        _logger = logger;
+    }
 
-        public ServiceDiscoveryHostedService(IConsulClient client, ServiceConfig config, ILogger<ServiceDiscoveryHostedService> logger)
+    public async Task StartAsync(CancellationToken cancellationToken)
+    {
+        try
         {
-            _client = client;
-            _config = config;
-            _logger = logger;
-        }
-
-        public async Task StartAsync(CancellationToken cancellationToken)
-        {
-            try
+            _registration = new AgentServiceRegistration
             {
-                _registration = new AgentServiceRegistration
+                ID = _config.Id,
+                Name = _config.Name,
+                Address = _config.Address,
+                Port = _config.Port,
+                Check = new AgentServiceCheck()
                 {
-                    ID = _config.Id,
-                    Name = _config.Name,
-                    Address = _config.Address,
-                    Port = _config.Port,
-                    Check = new AgentServiceCheck()
-                    {
-                        DeregisterCriticalServiceAfter = TimeSpan.FromSeconds(5),
-                        Interval = TimeSpan.FromSeconds(10),
-                        HTTP = _config.HealthCheckEndPoint,
-                        Timeout = TimeSpan.FromSeconds(10),
-                    },
-                };
+                    DeregisterCriticalServiceAfter = TimeSpan.FromSeconds(5),
+                    Interval = TimeSpan.FromSeconds(10),
+                    HTTP = _config.HealthCheckEndPoint,
+                    Timeout = TimeSpan.FromSeconds(10),
+                },
+            };
 
-                // Deregister already registered service
-                await _client
-                    .Agent.ServiceDeregister(_registration.ID, cancellationToken)
-                    .ConfigureAwait(false);
+            // Deregister already registered service
+            await _client
+                .Agent.ServiceDeregister(_registration.ID, cancellationToken)
+                .ConfigureAwait(false);
 
-                // Registers service
-                await _client.Agent.ServiceRegister(_registration, cancellationToken).ConfigureAwait(false);
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError($"Registering Blog Microservice error: {ex.ToString()}");
-            }
+            // Registers service
+            await _client.Agent.ServiceRegister(_registration, cancellationToken).ConfigureAwait(false);
         }
-
-        public async Task StopAsync(CancellationToken cancellationToken)
+        catch (Exception ex)
         {
-            //var registration = new AgentServiceRegistration { ID = _config.Id };
-
-            //await _client
-            //    .Agent.ServiceDeregister(_registration.ID, cancellationToken)
-            //    .ConfigureAwait(false);
+            _logger.LogError($"Registering Blog Microservice error: {ex.ToString()}");
         }
+    }
+
+    public async Task StopAsync(CancellationToken cancellationToken)
+    {
+        //var registration = new AgentServiceRegistration { ID = _config.Id };
+
+        //await _client
+        //    .Agent.ServiceDeregister(_registration.ID, cancellationToken)
+        //    .ConfigureAwait(false);
     }
 }
