972f6c60b00c6e3e9eadd5e1db0964af08e4a88e
Lin Thit Htoo <lth1212001@gmail.com>
2025-05-28 15:39:04 +0630

    -

--- swarm_microservices.blog/Extensions/DependencyInjectionExtensions.cs
+++ swarm_microservices.blog/Extensions/DependencyInjectionExtensions.cs
@@ -2,62 +2,69 @@
 using swarm_microservices.auth.Configurations;
 using swarm_microservices.auth.Services;
 
-namespace swarm_microservices.auth.Extensions
+namespace swarm_microservices.auth.Extensions;
+
+public static class DependencyInjectionExtensions
 {
-    public static class DependencyInjectionExtensions
+    public static IServiceCollection AddDependencies(
+        this IServiceCollection services,
+        WebApplicationBuilder builder
+    )
     {
-        public static IServiceCollection AddDependencies(this IServiceCollection services, WebApplicationBuilder builder)
+        if (builder.Environment.IsStaging())
         {
-            if (builder.Environment.IsStaging())
-            {
-                builder.Services.AddConsul(builder.Configuration.GetServiceConfig());
-            }
-
-            builder.Configuration.SetBasePath(builder.Environment.ContentRootPath)
-                .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: false, reloadOnChange: true)
-                .AddEnvironmentVariables();
-
-            builder.Services.AddControllers();
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-            builder.Services.AddHostedService<ServiceDiscoveryHostedService>();
-            builder.Services.AddHealthChecks();
-
-            return services;
+            builder.Services.AddConsul(builder.Configuration.GetServiceConfig());
         }
 
-        public static ServiceConfig GetServiceConfig(this IConfiguration configuration)
+        builder
+            .Configuration.SetBasePath(builder.Environment.ContentRootPath)
+            .AddJsonFile(
+                $"appsettings.{builder.Environment.EnvironmentName}.json",
+                optional: false,
+                reloadOnChange: true
+            )
+            .AddEnvironmentVariables();
+
+        builder.Services.AddControllers();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+        builder.Services.AddHostedService<ServiceDiscoveryHostedService>();
+        builder.Services.AddHealthChecks();
+
+        return services;
+    }
+
+    public static ServiceConfig GetServiceConfig(this IConfiguration configuration)
+    {
+        if (configuration is not null)
         {
-            if (configuration is not null)
+            var serviceConfig = new ServiceConfig
             {
-                var serviceConfig = new ServiceConfig
-                {
-                    Id = configuration.GetValue<string>("Consul:Id"),
-                    Name = configuration.GetValue<string>("Consul:Name"),
-                    Address = configuration.GetValue<string>("Consul:Address"),
-                    Port = configuration.GetValue<int>("Consul:Port"),
-                    DiscoveryAddress = configuration.GetValue<Uri>("Consul:DiscoveryAddress"),
-                    HealthCheckEndPoint = configuration.GetValue<string>("Consul:HealthCheckEndPoint"),
-                };
+                Id = configuration.GetValue<string>("Consul:Id"),
+                Name = configuration.GetValue<string>("Consul:Name"),
+                Address = configuration.GetValue<string>("Consul:Address"),
+                Port = configuration.GetValue<int>("Consul:Port"),
+                DiscoveryAddress = configuration.GetValue<Uri>("Consul:DiscoveryAddress"),
+                HealthCheckEndPoint = configuration.GetValue<string>("Consul:HealthCheckEndPoint"),
+            };
 
-                return serviceConfig;
-            }
-
-            throw new ArgumentNullException(nameof(configuration));
+            return serviceConfig;
         }
 
-        public static void AddConsul(this IServiceCollection services, ServiceConfig serviceConfig)
+        throw new ArgumentNullException(nameof(configuration));
+    }
+
+    public static void AddConsul(this IServiceCollection services, ServiceConfig serviceConfig)
+    {
+        ArgumentNullException.ThrowIfNull(serviceConfig);
+
+        var consulClient = new ConsulClient(config =>
         {
-            ArgumentNullException.ThrowIfNull(serviceConfig);
+            config.Address = serviceConfig.DiscoveryAddress;
+        });
 
-            var consulClient = new ConsulClient(config =>
-            {
-                config.Address = serviceConfig.DiscoveryAddress;
-            });
-
-            services.AddSingleton(serviceConfig);
-            services.AddSingleton<IConsulClient, ConsulClient>(_ => consulClient);
-            services.AddHostedService<ServiceDiscoveryHostedService>();
-        }
+        services.AddSingleton(serviceConfig);
+        services.AddSingleton<IConsulClient, ConsulClient>(_ => consulClient);
+        services.AddHostedService<ServiceDiscoveryHostedService>();
     }
 }
